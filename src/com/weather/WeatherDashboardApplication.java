b024fefab493b4a75ae186e87a1953b50b8ca07c
mitra-sh <51207047+mitra-sh@users.noreply.github.com>
2021-02-28 20:01:45 +0330

    Update WeatherDashboardApplication.java

--- src/com/weather/WeatherDashboardApplication.java
+++ src/com/weather/WeatherDashboardApplication.java
@@ -1,3 +1,8 @@
+/*
+I certify that this assignment/report is my own work, based on my personal study and/or research. I also certify
+that I have not copied in part or whole or otherwise plagiarized the work of other students and/or persons. I also
+certify that I will not share my solution with others nor make it publicly available online..
+*/
 package com.weather;
 
 import com.weather.XmlParser.ContinuousWeatherParser;
